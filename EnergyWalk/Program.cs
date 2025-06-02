472021c36095971a7ddaeebdb7feecbcb87831d9
Alok-Roy-Vamk <royalokeis343@gmail.com>
2025-06-02 17:59:41 +0300

    codes for service configuration

--- EnergyWalk/Program.cs
+++ EnergyWalk/Program.cs
@@ -1,44 +1,69 @@
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+// Add services to the container  
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+// Configure middleware  
+ConfigureMiddleware(app);
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+// Map endpoints  
+app.MapControllers();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// Method to configure services  
+void ConfigureServices(IServiceCollection services)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Energy Management API",
+            Version = "v1",                       
+        });
+    });
+   
+    services.AddControllers();
+
+    // Add Authentication   todo: if needed
+
+    // Add Authorization    todo: if needed
+}
+
+// Method to configure middleware  
+void ConfigureMiddleware(WebApplication app)
+{
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+    else
+    {
+        app.UseExceptionHandler(errorApp =>
+        {
+            errorApp.Run(async context =>
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                context.Response.ContentType = "application/json";
+
+                var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                var errorResponse = new
+                {
+                   // Message = AppConstants.Message.UnexpectedError,
+                    Detail = exceptionHandlerPathFeature?.Error.Message
+                };
+                await context.Response.WriteAsync(JsonSerializer.Serialize(errorResponse));
+            });
+        });
+    }
 }
